4fa071b46fc9f5d504b0ab5a6abb845b1624f882
Sami Suteria <sami.suteria@gmail.com>
2015-08-22 16:01:19 -0700

    Initial Commit

---
+++ Study Abroad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study Abroad
+//
+//  Created by Sami Suteria on 8/22/15.
+//  Copyright (c) 2015 Limerick Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Study Abroad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Study Abroad
+//
+//  Created by Sami Suteria on 8/22/15.
+//  Copyright (c) 2015 Limerick Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
